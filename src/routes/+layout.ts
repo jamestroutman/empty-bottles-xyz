2f349109fd1816ac1245cffd973bb25f5000f9fd
James Troutman <jtroutman@qrails.com>
2024-04-03 20:33:57 -0500

    Vercel Web Analytics p2

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,9 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import { inject } from '@vercel/analytics'
+import { dev } from '$app/environment';
+
+inject({ mode: dev ? 'development' : 'production' });
 
 export const load = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
